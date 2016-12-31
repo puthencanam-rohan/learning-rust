ca8ad6586cb5cf418d8c151eaaac34fadf7d13c6
puthencanam-rohan <puthencanam.rohan@gmail.com>
2016-12-30 21:15:31 -0500

    Guessing game with random numbers and looping.

--- code/guessing-game/src/main.rs
+++ code/guessing-game/src/main.rs
@@ -1,15 +1,40 @@
+extern crate rand;
+
 // The standard library + io module
 use std::io;
+// Random
+use rand::Rng;
+// Comparisons
+use std::cmp::Ordering;
 
 fn main() -> () {
     println!("Guess the number!");
 
-    println!("Please input your guess.");
+    let secret_nr = rand::thread_rng().gen_range(1, 101);
 
-    let mut guess = String::new();
+    loop {
 
-    io::stdin().read_line(&mut guess)
-        .expect("Failed to read line");
+        println!("Please input your guess.");
 
-    println!("You guessed: {}", guess);
+        let mut guess = String::new();
+
+        io::stdin().read_line(&mut guess)
+            .expect("Failed to read line");
+
+        let guess: u32 = guess.trim().parse()
+            .expect("Please type a number!");
+
+        println!("You guessed: {}", guess);
+
+        match guess.cmp(&secret_nr) {
+            Ordering::Less      => println!("Too small!"),
+            Ordering::Greater   => println!("Too big!"),
+            Ordering::Equal     => {
+                                    println!("You win!");
+                                    break;
+                                },
+        }
+
+    }
 }
+
